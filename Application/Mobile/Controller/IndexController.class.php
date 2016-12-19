df2ef9c6af6795476a49ad02a334baf7905a4591
jie123920 <1204748809@qq.com>
2016-12-19 19:30:00 +0800

    Add files via upload

--- Application/Mobile/Controller/IndexController.class.php
+++ Application/Mobile/Controller/IndexController.class.php
@@ -1,23 +1,19 @@
 <?php
 /**
- * 首页
+ * tpshop
+ * ============================================================================
+ * * 版权所有 2015-2027 深圳搜豹网络科技有限公司，并保留所有权利。
+ * 网站地址: http://www.tp-shop.cn
+ * ----------------------------------------------------------------------------
+ * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和使用 .
+ * 不允许对程序代码以任何形式任何目的的再发布。
+ * ============================================================================
+ * $Author: JY 2016-01-09
  */ 
 namespace Mobile\Controller;
 class IndexController extends MobileBaseController {
 
     public function index(){
-       /* $model = M('modelusers')->where(array('mobile_validated'=>1))->select();
-        //$user_id = array();
-        $arr = array();
-        foreach($model as $k=>$v){
-            $user_id = $v['user_id'];
-            $img_arr = M('user_images')->where(array('modeluser_id'=>$user_id))->order("img_id desc")->limit(1)->find();
-            $arr[] = $img_arr;
-            $arr[$k]['nickname'] = $v['nickname'];
-        }
-        //var_dump($arr);exit;
-        $this->assign('arr',$arr);*/
-    	//轮播图
         $list = M('lunbo')->where(array('is_show'=>1))->limit(3)->select();
         $this->assign('list',$list);
         $this->display();
@@ -27,6 +23,19 @@ class IndexController extends MobileBaseController {
      * 红人
      */
     public function hotPerson(){
+        $id = $_GET['id'];
+        $s_arr = M('schedule')->select();
+        $arr = array();
+        foreach($s_arr as $k=>$v){
+            $arr[$k] = $v['id'];
+        }
+        if($id==''){
+            $id=max($arr);
+        }
+        //var_dump($id);exit;
+        $m_arr = M('schedule')->where(array('id'=>$id))->find();
+        $this->assign('m_arr',$m_arr);
+        $this->assign('s_arr',$s_arr);
         $this->display();
     }
 
@@ -60,21 +69,43 @@ class IndexController extends MobileBaseController {
         $this->display();
 
     }
-    /*
-     * 首页模特展示 
-    */
     public function ajaxGetMore(){
         $p = I('p',1);
         $model = M('modelusers')->where(array('mobile_validated'=>1))->page($p,9)->select();
-        $arr = array();
+        /*$arr = array();
         foreach($model as $k=>$v){
             $user_id = $v['user_id'];
             $img_arr = M('user_images')->where(array('modeluser_id'=>$user_id))->order("img_id desc")->limit(1)->find();
             $arr[] = $img_arr;
             $arr[$k]['nickname'] = $v['nickname'];
-        }
+        }*/
         //var_dump($arr);exit;
+        $this->assign('arr',$model);
+        $this->display();
+    }
+
+    /**
+     * 加载更多红人
+     */
+    public function ajaxMore(){
+        $p = I('p',1);
+        $id = $_GET['id'];
+        $m_arr = M('schedule')->where(array('id'=>1))->find();
+        $mod = explode(',',$m_arr['models']);
+        //var_dump($mod);exit;
+        $arr = array();
+        foreach($mod as $k=>$v){
+            $arr[$k] = M('modelusers')->where(array('user_id'=>$v))->find();
+        }
+        foreach($arr as $k=>$v){
+            $head_pic = $v['head_pic'];
+            $experience = $v['experience'];
+            $nickname = $v['nickname'];
+        }
         $this->assign('arr',$arr);
+
+        $model = M('modelusers')->where(array('mobile_validated'=>1))->order('total desc')->page($p,2)->select();
+        $this->assign('model',$model);
         $this->display();
     }
 }
